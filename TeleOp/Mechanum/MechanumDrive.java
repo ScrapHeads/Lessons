2b57d8248ea1d7467eb20fd3542f50e8f6b2e4de
C34343 <96064635+C34343@users.noreply.github.com>
2023-08-13 16:21:12 -0500

    Update MechanumDrive.java

--- TeleOp/Mechanum/MechanumDrive.java
+++ TeleOp/Mechanum/MechanumDrive.java
@@ -27,6 +27,9 @@ public class MechanumDrive extends OpMode {
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         rightBack = hardwareMap.get(DcMotor.class, "rightBack");
 
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
         // Displays on the drivers hub when it finishes initialization
         telemetry.addData("status", "Initialized");
     }
