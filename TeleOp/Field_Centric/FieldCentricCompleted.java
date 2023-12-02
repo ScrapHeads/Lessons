1020934b50b3ca27715c43a3b1af3cf9b8fd66c1
Christian Brock <96064635+C34343@users.noreply.github.com>
2023-12-02 15:01:06 -0600

    Create FieldCentricCompleted.java

---
+++ TeleOp/Field_Centric/FieldCentricCompleted.java
@@ -0,0 +1,116 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+import java.lang.reflect.Parameter;
+
+
+//Tells the robot that it's in teleop, and the name that will show up on the drivers hub
+@TeleOp(name="FieldCentricCompleted", group="FTCLessons")
+// Makes the program not show up on the drivers hub, comment this out if you want to use the code
+@Disabled
+public class FieldCentricCompleted extends OpMode {
+    private static DcMotor leftFront;
+    private static DcMotor leftBack;
+    private static DcMotor rightFront;
+    private static DcMotor rightBack;
+		IMU Gyro = null;
+    Orientation Angles = null;
+    double offset = 0.0; //If you use offset and is greater than 0 make it negative
+
+    public double[] calculateDrive(double forward, double slide, double turn, double heading) {
+        double Temp = forward * Math.cos(heading) + slide * Math.sin(heading);
+        slide = (-forward * Math.sin(heading)) + slide * Math.cos(heading);
+        forward = Temp;
+
+        double leftFrontPower = forward + slide + turn;
+        double leftBackPower = forward - slide + turn;
+        double rightFrontPower = forward - slide - turn;
+        double rightBackPower = forward + slide - turn;
+
+        double biggestInput = Math.max(Math.abs(rightFrontPower), Math.abs(leftFrontPower));
+        biggestInput = Math.max(biggestInput, Math.abs(leftBackPower));
+        biggestInput = Math.max(biggestInput, Math.abs(rightBackPower));
+
+        if (biggestInput > 1) {
+            leftFrontPower = leftFrontPower / biggestInput;
+            leftBackPower = leftBackPower / biggestInput;
+            rightFrontPower = rightFrontPower / biggestInput;
+            rightBackPower = rightBackPower / biggestInput;
+        }
+
+        double[] powers = {leftFrontPower, leftBackPower, rightFrontPower, rightBackPower};
+
+        return powers;
+
+    }
+  
+    // performs once when you hit the init button on the drivers hub
+    @Override
+    public void init() {
+        // Finds the hardware who's configured name matches the name in the string
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+				Gyro = hardwareMap.get(IMU.class, "imu");
+				Orientation orientation =
+                new Orientation(AxesReference.INTRINSIC,
+                        AxesOrder.ZYX,
+                        AngleUnit.DEGREES,
+                        180, 0, 0, 0);
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(orientation);
+        IMU.Parameters parameters = new IMU.Parameters(orientationOnRobot);
+        Gyro.initialize(parameters);
+
+
+        // Displays on the drivers hub when it finishes initialization
+        telemetry.addData("status", "Initialized");
+    }
+
+    // called constantly while the program is running
+    @Override
+    public void loop() {
+        double forwards = -gamepad1.left_stick_y;
+        double horizontal = gamepad1.left_stick_x;
+        double turning = gamepad1.right_stick_x;
+
+        YawPitchRollAngles robotOrientation;
+        robotOrientation = Gyro.getRobotYawPitchRollAngles();
+        double yaw = robotOrientation.getYaw(AngleUnit.RADIANS);
+        double pitch = robotOrientation.getPitch(AngleUnit.RADIANS);
+        double roll = robotOrientation.getRoll(AngleUnit.RADIANS);
+        double GyroRadians = -yaw + offset;
+
+        double[] powers = calculateDrive(forwards, horizontal, turning, GyroRadians);
+
+        leftFront.setPower(powers[0]);
+        leftBack.setPower(powers[1]);
+        rightFront.setPower(powers[2]);
+        rightBack.setPower(powers[3]);
+
+        telemetry.addData("Motors", "leftFrontWheel:" + leftFront.getPower());
+        telemetry.addData("Motors", "leftBackWheel:" + leftBack.getPower());
+        telemetry.addData("Motors", "rightFrontWheel:" + rightFront.getPower());
+        telemetry.addData("Motors", "rightBackWheel:" + rightBack.getPower());
+        telemetry.addData("Input", "left y:" + gamepad1.left_stick_y +
+                ", left x:" + gamepad1.left_stick_x +
+                ", right x:" + gamepad1.right_stick_x);
+    }
+}
